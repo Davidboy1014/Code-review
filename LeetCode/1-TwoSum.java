acf7cf78973b2135d9bc0f02f9e1bc9d789eed41
David Cavan <DavidCavan30@gmail.com>
2025-07-10 14:30:25 -0500

    Attempted Twosum explained my thought proccess --- Will attempt more later

--- LeetCode/1-TwoSum.java
+++ LeetCode/1-TwoSum.java
@@ -24,13 +24,41 @@ Output: [0,1]
 
 Constraints:
 
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
+2 <= nums.length <= 10^4
+-10^9 <= nums[i] <= 10^9
+-10^9 <= target <= 10^9
 Only one valid answer exists.
  
 
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
  */
+/*
+ * Thinking proccess:
+ * Since there is always one answer in each array lets create a hashmap to first
+ *  run a for loop and store all integer values along with their place in a a hashmap <int (Value), int (place)>
+ *  then start from the beginning of the array and subtract it from the target number giving us the value to search for in the Hashmap
+ *  4 | 5 | 10 | 7 | 8 | 2 Target 9
+ */
+import java.util.HashMap;
 
- 
+ class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> num = new HashMap<>();
+        //First for loop checks to make sure the value in the array is not bigger than the target,
+        // if is not it will add it to hashmap <int (value), int (place)>.
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] <= target){
+                num.put(nums[i],i);
+            }
+        }
+
+        for(int i = 0; i <nums.length; i++){
+            int tar = target - nums[i];
+            if (num.containsValue(tar)){
+                //return int[i,num[i]];
+            }
+        }
+        return nums;
+    }
+}
+
