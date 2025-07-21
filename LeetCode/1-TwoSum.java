25782b392b585f4a8cb3f9330cfe21e5cc34898a
David Cavan <DavidCavan30@gmail.com>
2025-07-20 20:54:24 -0500

    Added to Twosum Leetcode.

--- LeetCode/1-TwoSum.java
+++ LeetCode/1-TwoSum.java
@@ -37,13 +37,14 @@ Follow-up: Can you come up with an algorithm that is less than O(n2) time comple
  * Since there is always one answer in each array lets create a hashmap to first
  *  run a for loop and store all integer values along with their place in a a hashmap <int (Value), int (place)>
  *  then start from the beginning of the array and subtract it from the target number giving us the value to search for in the Hashmap
- *  4 | 5 | 10 | 7 | 8 | 2 Target 9
+ *  4 | 5 | 10 | 7 | 8 | 2 Target 9 Therefore we can just do a simple hashlookup to recieve the place
  */
 import java.util.HashMap;
+import java.util.Map;
 
  class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> num = new HashMap<>();
+        Map<Integer, Integer> num = new HashMap<>();
         //First for loop checks to make sure the value in the array is not bigger than the target,
         // if is not it will add it to hashmap <int (value), int (place)>.
         for(int i = 0; i < nums.length; i++){
@@ -54,8 +55,8 @@ import java.util.HashMap;
 
         for(int i = 0; i <nums.length; i++){
             int tar = target - nums[i];
-            if (num.containsValue(tar)){
-                //return int[i,num[i]];
+            if (num.containsKey(tar)){
+                return new int[]{num.get(tar),i};
             }
         }
         return nums;
