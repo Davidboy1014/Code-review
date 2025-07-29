a486c11d4e1eccbe4da4dc04733e8a32562c50c2
DCavan <Dcavan@iastate.edu>
2025-07-29 11:50:31 -0500

    Commiting to sync with both of my computers

---
+++ Diceroller/Test/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+    public static void main(String[] args) {
+        String TesString = "Hello";
+        int TesInt  = 20;
+        System.out.println(TesInt);
+
+    }
+    
+}
